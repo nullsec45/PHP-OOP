ad76699a4b21009bfd931e60f4436913a0a2dfc5
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-15 17:09:39 +0700

    Properties

---
+++ Properties.php
@@ -0,0 +1,12 @@
+<?php
+require_once "data/Person.php";
+
+$person=new Person();
+$person->name="Rama Fajar Fadhillah";
+$person->address="Jl.Kondangan 1";
+$person->country="Indonesia";
+var_dump($person);
+
+echo "Nama     : $person->name".PHP_EOL;
+echo "Address  : $person->address".PHP_EOL;
+echo "Country  : $person->country".PHP_EOL;
--- data/Person.php
+++ data/Person.php
@@ -1,11 +1,13 @@
 <?php
 class Person{
-
+    var $name;
+    var $address;
+    var $country;   
 }
 class Mobil{
 
 }
 class Motor{
-    
+
 }
 ?>
