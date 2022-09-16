eee5e905646e46e3e3e73c516c46e43f6b0ada32
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 22:53:28 +0700

    Function Override

--- data/Manager.php
+++ data/Manager.php
@@ -3,12 +3,14 @@ class Manager{
     var string $name;
 
     function sayHello(string $name):void{
-        echo "Hi $name, my name is $this->name".PHP_EOL;
+        echo "Hi $name, my name is manager $this->name".PHP_EOL;
     }
 }
 
 class VicePrecident extends Manager{
-
+    function sayHello(string $name):void{
+        echo "Hi $name, my name is vice precident $this->name".PHP_EOL;
+    }
 }
 
 ?>
