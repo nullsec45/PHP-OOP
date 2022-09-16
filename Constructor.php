9ed321292151766910f52fd4504a0a7f190a2f8f
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 20:56:51 +0700

    Constructor

---
+++ Constructor.php
@@ -0,0 +1,5 @@
+<?php
+require_once "data/Person.php";
+$rama=new Person("Rama","Jakarta");
+
+var_dump($rama);
--- data/Person.php
+++ data/Person.php
@@ -6,6 +6,11 @@ class Person{
     var ?string $address;
     var string $country="Indonesia";   
 
+    function __construct(string $name, ?string $address)
+    {
+        $this->name=$name;
+        $this->address=$address;
+    }
     function sayHello(?string $name){
         if(is_null($name)){
             echo "Hi, my name is $this->name".PHP_EOL;
