1675b078abf2665b47b5a0dd111e423e1a23b13e
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 21:34:12 +0700

    Global Namespace

--- Namespace.php
+++ Namespace.php
@@ -1,9 +1,11 @@
 <?php
-require_once "data/Conflict.php";
-require_once "data/Helper.php";
+namespace{
+    require_once "data/Conflict.php";
+    require_once "data/Helper.php";
 
-$conflict1=new Data\Conflict1\Conflict();
-$conflict2=new Data\Conflict2\Conflict();
+    $conflict1=new Data\Conflict1\Conflict();
+    $conflict2=new Data\Conflict2\Conflict();
 
-echo Helper\APPLICATION.PHP_EOL;
-Helper\helpMe();
+    echo Helper\APPLICATION.PHP_EOL;
+    Helper\helpMe();
+}
