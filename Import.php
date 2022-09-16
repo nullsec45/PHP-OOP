73500d8463ed70bc677c8b77ac945e5c1946c0d7
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 21:44:15 +0700

    import dan use keyword

---
+++ Import.php
@@ -0,0 +1,15 @@
+<?php
+require_once "data/Conflict.php";
+require_once "data/Conflict.php";
+
+// Walau nama namespacenya berbeda, tidak bisa memanggil class yang sama menggunakan use keyword;
+use Data\Conflict1\Conflict;
+// use Data\Conflict2\Conflict; // akan error
+use function Helper\helpMe;
+use const Helper\APPLICATION;
+
+$conflict=new Conflict();
+// Jika tetap ingin menggunakan class yang sama, harus memanggil namespacenya di instancenya
+$conflict2=new Data\Conflict2\Conflict();
+helpMe();
+echo APPLICATION.PHP_EOL;
