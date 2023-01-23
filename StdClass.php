b306dab32113a3bda4e543b5a3f3fa37079ff359
ramarff <rama21010ti@student.nurulfikri.ac.id>
2023-01-23 17:16:31 +0700

    Std Class

--- StdClass.php
+++ StdClass.php
@@ -1,14 +1,22 @@
 <?php
 
+// StdClass digunakan untuk konversi tipe data array ke object
 $array=["firstName" => "Rama","middleName" => "Fajar", "lastName" => "Fadhillah"];
 
 $object=(object)$array;
 
-var_dump($object);
+// var_dump($object);
 
-echo "First Name $object->firstName".PHP_EOL;
-echo "Middle Name $object->middleName".PHP_EOL;
-echo "Last Name $object->lastName".PHP_EOL;
+// echo "First Name $object->firstName".PHP_EOL;
+// echo "Middle Name $object->middleName".PHP_EOL;
+// echo "Last Name $object->lastName".PHP_EOL;
 
-$arrayLagi=(array)$object;
-var_dump($arrayLagi);
+$arrayLagi=(array)$person;
+// var_dump($arrayLagi);
+
+require_once "data/Person.php";
+$person=new Person("Fajar","Jakarta");
+var_dump($person);
+
+$arrayPerson=(array) $person;
+var_dump($arrayPerson);
